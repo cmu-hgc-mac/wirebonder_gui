792583fff8cc05ce2eb3a1816d663e3eeffb7640
Sindhu Murthy <58646122+murthysindhu@users.noreply.github.com>
2024-11-29 10:59:39 -0500

    Create conn_example.py

---
+++ config/conn_example.py
@@ -0,0 +1,8 @@
+host = 'localhost' ## change this
+password = 'hgcal' ## change this
+
+inst_code = 'ME' ## CM, IH, NT, TI, TT, SB
+institution_name = 'Carnegie Mellon University' ## change this
+
+database = 'hgcdb'
+user = 'wirebond_user'
