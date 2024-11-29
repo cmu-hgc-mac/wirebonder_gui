02a666f398d17f0925e20caa01b88d0038bbbdd0
Sindhu Murthy <58646122+murthysindhu@users.noreply.github.com>
2024-11-29 11:00:13 -0500

    Create graphics_config_example.py

---
+++ config/graphics_config_example.py
@@ -0,0 +1,15 @@
+button_font_size = 9
+text_font_size = 14
+#window width and height
+w_width = 1360
+w_height = 1000
+#additional y offset
+add_y_offset = 0
+#additional x offset for HD
+add_x_offset = 0
+
+
+#dimensions of scroll areas
+#doesn't need to be changed
+scroll_width = 1450
+scroll_height = 1500
