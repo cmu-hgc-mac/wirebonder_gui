49372b066ec3ea4da56e6d8f74f3a8f2eacd9a6a
Sindhu Murthy <58646122+murthysindhu@users.noreply.github.com>
2024-11-29 19:38:44 -0500

    Update conn_example.py

--- config/conn_example.py
+++ config/conn_example.py
@@ -4,6 +4,6 @@ password = 'hgcal' ## change this
 inst_code = 'ME' ## CM, IH, NT, TI, TT, SB
 institution_name = 'Carnegie Mellon University' ## change this
 
-user = 'wirebond_user' ### or writer
+user = 'wirebond_user' ### or 'editor' for global write privileges to all tables.
 database = 'hgcdb'
 
