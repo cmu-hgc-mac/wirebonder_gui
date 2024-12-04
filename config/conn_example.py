d8449bcd48c5a03ba9e12b5db565c053cd5c275d
Sindhu Murthy <58646122+murthysindhu@users.noreply.github.com>
2024-12-03 21:25:55 -0500

    Update conn_example.py

--- config/conn_example.py
+++ config/conn_example.py
@@ -1,6 +1,5 @@
 host = 'localhost' ## change this
 password = 'hgcal' ## change this
-autosize = True
 
 inst_code = 'ME' ## CM, IH, NT, TI, TT, SB
 institution_name = 'Carnegie Mellon University' ## change this
