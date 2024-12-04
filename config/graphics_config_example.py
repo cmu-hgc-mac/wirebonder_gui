49856e1d4aea2b79892da081a7900743a17da2d6
Sindhu Murthy <58646122+murthysindhu@users.noreply.github.com>
2024-12-03 21:26:27 -0500

    Update graphics_config_example.py

--- config/graphics_config_example.py
+++ config/graphics_config_example.py
@@ -3,6 +3,7 @@ text_font_size = 14
 w_width = 1360  # window width and height
 w_height = 1000
 
+autosize = True  ## Set False and add your own offsets if needed.
 add_y_offset = 0  # additional y offset
 add_x_offset = 0  # additional x offset for HD
 
