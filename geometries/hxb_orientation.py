4373329f1a90e1406a4ffc2f854831310d23d281
aletheabn <aletheabn@ucsb.edu>
2025-02-11 12:13:36 -0800

    Update hxb_orientation.py
    fix typo, alethea edits

--- geometries/hxb_orientation.py
+++ geometries/hxb_orientation.py
@@ -1,14 +1,14 @@
 hxb_orientation = {
-    'HF': {'rot_ang_fr' : 0, 'rot_ang_bk' :   0, 'load_pin' : [251,6]}, 
-    'HL': {'rot_and_fr' : 0, 'rot_ang_bk' : 180, 'load_pin' : [239,234]},
+    'HF': {'rot_ang_fr' : 0, 'rot_ang_bk' :   0, 'load_pin' : [229,34]}, 
+    'HL': {'rot_ang_fr' : 0, 'rot_ang_bk' : 180, 'load_pin' : [239,234]},
     'HR': {'rot_ang_fr' : 0, 'rot_ang_bk' : 180, 'load_pin' : [247,254]},
-    'HT': {'rot_ang_fr' : 0, 'rot_ang_bk' : 180, 'load_pin' : [81,6]}, 
+    'HT': {'rot_ang_fr' : 0, 'rot_ang_bk' : 180, 'load_pin' : [148.5,21]}, 
     'HB': {'rot_ang_fr' : 0, 'rot_ang_bk' :   0, 'load_pin' : [216,431]},
     
     'LF': {'rot_ang_fr' : 0, 'rot_ang_bk' :   0, 'load_pin' : [103,13]}, 
     'LL': {'rot_ang_fr' : 0, 'rot_ang_bk' : 180, 'load_pin' : [109,104]}, 
     'LR': {'rot_ang_fr' : 0, 'rot_ang_bk' : 180, 'load_pin' : [112,117]}, 
     'LT': {'rot_ang_fr' : 0, 'rot_ang_bk' : 180, 'load_pin' : [61,15]}, 
-    'LB': {'rot_ang_fr' : 0, 'rot_ang_bk' :   0, 'load_pin' : [119,184]}, 
+    'LB': {'rot_ang_fr' : 0, 'rot_ang_bk' :   0, 'load_pin' : [110.5,185.5]}, 
     'L5': {'rot_ang_fr' : 0, 'rot_ang_bk' : 180, 'load_pin' : [110.5,4]}, 
 }
