78973a74ff5b4a155427a685d60b585555a4a903
aletheabn <aletheabn@ucsb.edu>
2025-02-11 12:22:09 -0800

    Update hxb_orientation.py

--- geometries/hxb_orientation.py
+++ geometries/hxb_orientation.py
@@ -1,6 +1,6 @@
 hxb_orientation = {
     'HF': {'rot_ang_fr' : 0, 'rot_ang_bk' :   0, 'load_pin' : [229,34]}, 
-    'HL': {'rot_ang_fr' : 0, 'rot_ang_bk' : 180, 'load_pin' : [239,234]},
+    'HL': {'rot_ang_fr' : 0, 'rot_ang_bk' : 180, 'load_pin' : [230,236]},
     'HR': {'rot_ang_fr' : 0, 'rot_ang_bk' : 180, 'load_pin' : [247,254]},
     'HT': {'rot_ang_fr' : 0, 'rot_ang_bk' : 180, 'load_pin' : [148.5,21]}, 
     'HB': {'rot_ang_fr' : 0, 'rot_ang_bk' :   0, 'load_pin' : [216,431]},
