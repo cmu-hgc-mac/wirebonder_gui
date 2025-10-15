03312caba0bb319abf54847321eff5557e60a9d0
Sindhu Murthy <sindhumurthy97@gmail.com>
2025-10-15 19:53:57 -0400

    request unique mby module_name instead of module_no with account for hyphens

--- modules/postgres_tools.py
+++ modules/postgres_tools.py
@@ -123,8 +123,8 @@ async def find_to_revisit(pool,):
     bad_modules = {}
 
     read_query = f"""WITH 
-            latest_fr_wb AS (SELECT DISTINCT ON (REPLACE(module_name, '-','') AS module_name) * FROM front_wirebond ORDER BY REPLACE(module_name, '-','') DESC, (date_bond + time_bond) DESC),
-            latest_bk_wb AS (SELECT DISTINCT ON (REPLACE(module_name, '-','') AS module_name) * FROM back_wirebond ORDER BY REPLACE(module_name, '-','') DESC, (date_bond + time_bond) DESC)
+            latest_fr_wb AS (SELECT DISTINCT ON (module_name) * FROM front_wirebond ORDER BY module_name DESC, (date_bond + time_bond) DESC),
+            latest_bk_wb AS (SELECT DISTINCT ON (module_name) * FROM back_wirebond ORDER BY module_name DESC, (date_bond + time_bond) DESC)
             SELECT REPLACE(mi.module_name, '-',''), fw.wb_fr_marked_done, bw.wb_bk_marked_done
             FROM module_info mi
             LEFT JOIN latest_fr_wb fw ON REPLACE(mi.module_name, '-','') = REPLACE(fw.module_name, '-','')
